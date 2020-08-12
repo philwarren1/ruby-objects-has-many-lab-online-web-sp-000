bc0e0569427401e3680ae4ef6d93b44fdcff3deb
Philip Warren <philwarren1@gmail.com>
2020-08-12 00:20:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,30 @@
 class Artist
   attr_accessor :name
-
   @@song_count = 0
-
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
+    @songs = [] 
   end
 
   def songs
-    @songs 
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
     @@song_count
   end
-end
+
+end 
